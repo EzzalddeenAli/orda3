d5258cce9468da4667688d60c831b558f4564eb0
Chang Liu <bobbyliu117@gmail.com>
2018-09-01 21:58:05 -0400

    bind request detail

--- base/src/main/java/faith/changliu/orda3/base/fragments/RequestFragment.kt
+++ base/src/main/java/faith/changliu/orda3/base/fragments/RequestFragment.kt
@@ -9,8 +9,8 @@ import android.view.ViewGroup
 import faith.changliu.orda3.base.BaseFragment
 import faith.changliu.orda3.base.R
 import faith.changliu.orda3.base.data.models.Request
-import faith.changliu.orda3.base.data.preferences.UserPref
 import faith.changliu.orda3.base.data.viewmodels.RequestViewModel
+import kotlinx.android.synthetic.main.fragment_request_detail_text_data.*
 import kotlinx.android.synthetic.main.fragment_requests.*
 import kotlinx.android.synthetic.main.fragment_requests_list.*
 import org.jetbrains.anko.support.v4.toast
@@ -20,20 +20,33 @@ class RequestsFragment : BaseFragment(), View.OnClickListener {
 	private val mViewModel by lazy { RequestViewModel() }
 	private lateinit var mRequestAdapter: RequestsAdapter
 
-	private val toDetail by lazy {
+	private val onUpdate by lazy {
 		if (include_request_detail == null)
 			{ request: Request ->
 				// todo: phone
 				toast("Phone: " + request.toString())
 				Unit
 			}
-		 else
+		else
+			{ request: Request ->
+				// todo: tablet
+				bind(request)
+			}
+	}
+
+	private val onDelete by lazy {
+		if (include_request_detail == null)
+			{ request: Request ->
+				// todo: phone
+				toast("Phone: " + request.toString())
+				Unit
+			}
+		else
 			{ request: Request ->
 				// todo: tablet
 				toast("Tablet: " + request.toString())
 				Unit
 			}
-
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -50,7 +63,7 @@ class RequestsFragment : BaseFragment(), View.OnClickListener {
 		mRcvRequests.layoutManager = LinearLayoutManager(context)
 
 		// todo: implement onUpdate, onDelete
-		mRequestAdapter = RequestsAdapter(arrayListOf(), toDetail)
+		mRequestAdapter = RequestsAdapter(arrayListOf(), onUpdate, onDelete)
 
 		// check which layout
 		if (include_request_detail == null) {
@@ -89,6 +102,28 @@ class RequestsFragment : BaseFragment(), View.OnClickListener {
 				// todo: enable
 //				addNew()
 			}
+		}
+	}
+
+	private fun bind(request: Request) {
+		mEtTitle.setText(request.title)
+		mEtDeadline.setText(request.deadline.toString())
+		mEtStatus.setText(getStatus(request.status))
+		mEtAssignedTo.setText(request.assignedTo)
+		mEtAddress.setText(request.address)
+		mEtCity.setText(request.city)
+		mEtCountry.setText(request.country)
+		mEtWeight.setText("${request.weight}")
+		mEtVolume.setText("${request.volume}")
+		mEtCompensation.setText("${request.compensation}")
+		mEtDescription.setText(request.description)
+	}
+
+	private fun getStatus(status: Int): String {
+		return when (status) {
+			0 -> "PENDING"
+			1 -> "ASSIGNED"
+			else -> "CLOSED"
 		}
 	}
 
--- base/src/main/java/faith/changliu/orda3/base/fragments/RequestsAdapter.kt
+++ base/src/main/java/faith/changliu/orda3/base/fragments/RequestsAdapter.kt
@@ -6,12 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import faith.changliu.orda3.base.R
 import faith.changliu.orda3.base.data.models.Request
-import faith.changliu.orda3.base.utils.snackConfirm
 import kotlinx.android.synthetic.main.cell_request.view.*
+import kotlin.properties.Delegates
 
 class RequestsAdapter(
 		var requests: ArrayList<Request>,
-		private val onClick: (Request) -> Unit
+		private val onUpdate: (Request) -> Unit,
+		private val onDelete: (Request) -> Unit
 ) : RecyclerView.Adapter<RequestsAdapter.ViewHolder>() {
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -29,13 +30,23 @@ class RequestsAdapter(
 
 	inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
+		private var isOpen by Delegates.observable(false) { _, _, newValue ->
+			itemView.mLayoutReveal.visibility = if (newValue) View.VISIBLE else View.GONE
+		}
+
 		fun bind(request: Request) {
 			itemView.apply {
 				mTvTitle.text = request.title
 				mTvMemo.text = request.description
+				mBtnUpdateRequest.setOnClickListener {
+					onUpdate(request)
+				}
+				mBtnDeleteRequest.setOnClickListener {
+					onDelete(request)
+				}
 
 				mCellView.setOnClickListener {
-					onClick(request)
+					isOpen = !isOpen
 				}
 			}
 		}
