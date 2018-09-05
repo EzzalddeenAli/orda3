5f3abd28e2fa7f087062ce348956cc3557d6b787
Chang Liu <bobbyliu117@gmail.com>
2018-09-05 14:38:28 -0400

    Fix sync data after login

--- agent/src/main/java/faith/changliu/orda3/agent/AgentLoginActivity.kt
+++ agent/src/main/java/faith/changliu/orda3/agent/AgentLoginActivity.kt
@@ -1,11 +1,16 @@
 package faith.changliu.orda3.agent
 
 import faith.changliu.orda3.base.activities.BaseLoginActivity
+import faith.changliu.orda3.base.data.AppRepository
+import faith.changliu.orda3.base.utils.tryBlock
 import faith.changliu.orda3.base.widgets.RegisterAgentDialog
 
 class AgentLoginActivity : BaseLoginActivity() {
 	override fun toMain() {
-		startActivitySingleTop(MainActivity::class.java)
+		tryBlock {
+			AppRepository.syncAll()
+			startActivitySingleTop(MainActivity::class.java)
+		}
 	}
 
 	override val mRegisterTextResId = R.string.register_agent
