6bfe21f7b9ee8eef85fcd84ae6ace6aa79d10345
Chang Liu <bobbyliu117@gmail.com>
2018-09-08 17:50:21 -0400

    Custom EditText layout add listener, etc.

--- base/src/main/java/faith/changliu/orda3/base/fragments/account/BaseAccountFragment.kt
+++ base/src/main/java/faith/changliu/orda3/base/fragments/account/BaseAccountFragment.kt
@@ -6,11 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import faith.changliu.orda3.base.BaseFragment
 import faith.changliu.orda3.base.R
+import kotlinx.android.synthetic.main.fragment_account_base.*
 
 open class BaseAccountFragment : BaseFragment() {
 	
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.fragment_account_base, container, false)
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		mCusEtZipcode.setOnTextChangeListener { s ->
+			if (s.length > 6) mCusEtZipcode.text = s.substring(0, 6)
+		}
 	}
 	
 }
--- base/src/main/java/faith/changliu/orda3/base/widgets/CusEditText.kt
+++ base/src/main/java/faith/changliu/orda3/base/widgets/CusEditText.kt
@@ -1,6 +1,8 @@
 package faith.changliu.orda3.base.widgets
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
@@ -13,21 +15,42 @@ class CusEditText @JvmOverloads constructor(
 	
 	var hint: String = ""
 	
+	/**
+	 * TYPE_CLASS_NUMBER = 0x00000002
+	 */
+	var inputType: Int
+	private var maxLength: Int
+	var text: String
+		get() = mEtInput.text.toString()
+		set(value) {
+			mEtInput.setText(value)
+		}
+	
 	init {
 		val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CusEditText)
 		hint = typedArray.getString(R.styleable.CusEditText_cus_et_hint)
+		inputType = typedArray.getInt(R.styleable.CusEditText_cus_et_inputType, 0x00000001)
+		maxLength = typedArray.getInt(R.styleable.CusEditText_cus_et_maxLength, 80)
 		
 		View.inflate(context, R.layout.cus_edit_text, this)
 		mEtLayout.hint = hint
+		mEtInput.inputType = inputType
+		mEtLayout.counterMaxLength = maxLength
 		
 		typedArray.recycle()
 	}
 	
-	fun setText(text: String) {
-		mEtInput.setText(text)
-	}
-	
-	fun getText(): String {
-		return mEtInput.text.toString()
+	fun setOnTextChangeListener(onTextChanged: (s: CharSequence) -> Unit) {
+		mEtInput.addTextChangedListener(object : TextWatcher {
+			override fun afterTextChanged(s: Editable?) {}
+			
+			override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+			
+			override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+				s?.let {
+					onTextChanged(it)
+				}
+			}
+		})
 	}
 }
